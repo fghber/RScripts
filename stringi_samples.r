2808d2cd1b12f74b80bbbdcda05497c912333cd2
fghber <wp78devel@live.com>
2020-05-15 20:49:37 -0500

    Update stringi_samples.r

--- stringi_samples.r
+++ stringi_samples.r
@@ -16,10 +16,8 @@ Orgs <- data.frame("OrgName" = c('WAIROA ABANDONNED COUNCIL',
                                  ' SMART ENVIORNMENTAL LTD', 
                                  'NEW ZEALAND INSTITUTE OF ABERATION ABILITYES'),
                    stringsAsFactors = FALSE)
-
 patterns <- readLines("bad.csv")
 replacements <- readLines("good.csv")
-
 for(i in 1:nrow(Orgs)) {
   row <- Orgs[i,]
   print(as.character(row))
@@ -30,20 +28,6 @@ for(i in 1:nrow(Orgs)) {
 }
 
 
-
-originalTXT<-"start abcd pat1 pat2 pat3 pat4"
-test.pat<-"pat1|pat2|pat3|(?=(?<=abcd)(.|\n)*pat4)"
-gregexpr(test.pat, originalTXT, perl=TRUE)
-
 stri_match_all_regex(originalTXT, "(pat1)|(pat2)|(pat3)|(?<=abcd)(.|\n)+(pat4)")
 overlappingMatch<-stri_match_all_regex(originalTXT,"(?<=abcd)(.|\n)+(pat4)")[[1]][,1]
 stri_match_all_regex(originalTXT, "pat1|pat2|pat3|pat4")
-
-
-
-s <- "height(female), weight, BMI, and BMI."
-stringr::str_replace(s, "(?<=,|^)([()\\w\\s]+),\\s(.*?)((?: and)?(?=\\1))", "\\2")
-
-street = c("2389 E Myronga Street NE")
-#sub(x = street, pattern = "\\s((?i)Street|(?i)St\\.?)(?=\\sNE)", replacement = " St", x, perl=TRUE)
-sub(x = street, pattern = "\\s((?i)Street|(?i)St\\.?)\\sNE", replacement = " St", perl=TRUE)
